d267039e0d75f52cdd21f8e8489f4e1da42ad5fc
horzsolt <horzsolt2006@gmail.com>
2018-09-23 20:52:49 +0200

    Add task executor example

---
+++ src/horzsolt/javaexamples/horzsolt/progressbar/Starter.java
@@ -0,0 +1,42 @@
+package horzsolt.javaexamples.horzsolt.progressbar;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.lang.RuntimeException;
+
+public class Starter {
+
+    public static void main(String[] args) {
+
+        AtomicLong numberOfTasksExecuted = new AtomicLong(0);
+        Supplier<Long> currentValue = () -> numberOfTasksExecuted.get();
+        int maxValue = 60;
+
+        Supplier<Long> tasks = () -> {
+            IntStream.range(0, maxValue).forEach(
+                    counter -> {
+                        System.out.println(counter);
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        numberOfTasksExecuted.incrementAndGet();
+                    }
+            );
+
+            return 1L;
+        };
+
+        TaskExecutor executor = new TaskExecutor(System.out, currentValue, maxValue);
+
+        try {
+            executor.start(tasks);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
